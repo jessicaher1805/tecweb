c501978908592054ca95f2e0bda8499a3d06ab66
Jessica Hernández <jessicacoronis@gmail.com>
2025-11-14 17:08:58 -0600

    Creaccion de la carpeta vendor

---
+++ practicas/p12/product_app/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jessicaher1805/productapp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f1cf0845440d5aeb8d94b1a53cfd8734141b540d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jessicaher1805/productapp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f1cf0845440d5aeb8d94b1a53cfd8734141b540d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
