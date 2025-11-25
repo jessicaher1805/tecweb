80e2c2e42f272729826936ae5e8f5199564d1f39
Jessica Hernández <jessicacoronis@gmail.com>
2025-11-24 19:23:47 -0600

    Ejercicio 3

--- practicas/p13/pruebaslim/index.php
+++ practicas/p13/pruebaslim/index.php
@@ -1,4 +1,8 @@
 <?php
+header("Content-Type: application/json");
+header("Accesa-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
 require 'vendor/autoload.php';
 
 $app = new Slim\App();
@@ -12,6 +16,16 @@ $app->get('/', function ($request, $response, $args){
 $app->get("/hola[/{nombre}]", function($request, $response, $args){
 
     $response->write("Hola,". $args["nombre"]);
+    return $response;
+});
+
+$app->post("/pruebapost", function($request, $response, $args){
+
+    $reqPost = $request->getParsedBody();
+    $val1 = $reqPost["val1"];
+    $val2 = $reqPost["val2"];
+
+    $response->write("Valores:" . $val1 ." ".$val2);
     return $response;
 });
 
